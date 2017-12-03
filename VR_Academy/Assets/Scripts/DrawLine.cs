561f258ea9a820f653bf6f369db9613b507a493f
Dylan Fox <dylan-fox@users.noreply.github.com>
2017-12-03 15:12:08 -0800

    Initiation

---
+++ VR_Academy/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+    private LineRenderer curLine;
+    private int numClicks = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Debug.LogError("Got here.");
+            GameObject go = new GameObject();
+            curLine = go.AddComponent<LineRenderer>();
+            curLine.SetWidth(0.1f, 0.1f);
+            numClicks = 0;
+        } else if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            curLine.positionCount = numClicks + 1;
+            curLine.SetPosition(numClicks, trackedObj.transform.position);
+            numClicks++;
+        }
+	}
+}
